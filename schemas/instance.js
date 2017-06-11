3e8515a53153cb5643bf5a593534b8cc959c85f6
Youngbok Yoon <master@bluehack.net>
2017-06-11 13:51:29 +0900

    [APIWAY-159] Added ‘tc’ field to save test result in the Instance schema

--- schemas/instance.js
+++ schemas/instance.js
@@ -2,25 +2,36 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var instanceSchema = new Schema({
-    project: {
-      name: String,
-      full_name: String,
-      projectId: String, //Project._id
-      home_url: String,
-      git_url: String,
-      provider: String,
-    },
-    owner: String, //User._id
-    startTime: Number,
-    endTime: Number,
-    locale: String,
-    status: String,
-    commit: String,
-    commitUrl: String,
-    branch: String,
-    logUrl: String,
-    reportJson: String,
-    reportHtml: String
+  project: {
+    name: String,
+    full_name: String,
+    projectId: String, //Project._id
+    home_url: String,
+    git_url: String,
+    provider: String,
+  },
+  owner: String, //User._id
+  startTime: Number,
+  endTime: Number,
+  locale: String,
+  status: String,
+  commit: String,
+  commitUrl: String,
+  branch: String,
+  logUrl: String,
+  reportJson: String,
+  reportHtml: String,
+  tc: {
+    suites: Number,
+    tests: Number,
+    passes: Number,
+    pending: Number,
+    failures: Number,
+    duration: Number,
+    skipped: Number,
+    passPercentClass: String,
+    pendingPercentClass: String
+  }
 });
 
 module.exports = instanceSchema;
